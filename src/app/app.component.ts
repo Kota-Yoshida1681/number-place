8a2dc3a50425d07337a2ecf628ef57c43ec1b0e5
ODYoshida <yoshida@objectdata.co.jp>
2024-09-17 23:44:57 +0900

    validationを追加

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,53 +18,125 @@ export class AppComponent implements OnInit, OnDestroy {
   ) {  }
 
   mode: Mode = "create";
-  forcus: number = -1;
+  forcusIndex: number = -1;
+  forcusNumber: InputNum = null;
   inputs: Square[][] = Array.from<unknown, Square[]>({length: 9}, () => Array.from<unknown, Square>({length: 9}, () => ({given: null, answer: null, memo: ''})));
+  outputs: InputNum[][] = Array.from<unknown, InputNum[]>({length: 9}, () => Array.from<unknown, InputNum>({length: 9}, () => null));
+  validations: boolean[] = Array.from<unknown, boolean>({length: 81}, () => true);
 
+  static calcSameFrameIndice(index: number): number[] {
+    return [0, 1, 2].map(i => i + Math.floor(index / 3)*3).filter(i => i !== index);
+  };
 
   changeMode(mode: Mode) {
     this.mode = mode;
   }
 
   clickSquare(number: number): void {
-    if (number === this.forcus) {
-      this.forcus = -1;
+    if (number === this.forcusIndex) {
+      this.forcusIndex = -1;
+      this.forcusNumber = null;
     }
     else {
-      this.forcus = number;
-    }
+      this.forcusIndex = number;
 
-    console.log(this.forcus);
+      const row = Math.floor(this.forcusIndex / 9);
+      const  line = this.forcusIndex % 9;
+      this.forcusNumber = this.outputs[row][line];
+    }
   }
 
   inputNum(event: KeyboardEvent): void {
-    if (this.forcus < 0) return;
+    if (this.forcusIndex < 0) return;
     if (this.mode === 'memo') return;
 
-    const num = Number(event.key) as InputNum;
-    if (Number.isNaN(num)) return;
+    const row = Math.floor(this.forcusIndex / 9);
+    const line = this.forcusIndex % 9;
 
-    const row = Math.floor(this.forcus / 9);
-    const  line = this.forcus % 9;
-
-    if (this.mode === 'create') {
-      this.inputs[row][line].given = num;
+    if (event.key === 'Backspace') {
+      if (this.mode === 'create') {
+        this.inputs[row][line].given = null;
+        this.outputs[row][line] = null;
+        this.forcusNumber = null;
+      }
+      else if (this.inputs[row][line].given === null) {
+        this.inputs[row][line].answer = null;
+        this.outputs[row][line] = null;
+        this.forcusNumber = null;
+      }
     }
     else {
-      this.inputs[row][line].answer = num;
+      const num = Number(event.key);
+      if (Number.isNaN(num) || num === 0) return;
+
+      if (this.mode === 'create') {
+        this.inputs[row][line].given = num as InputNum;
+        this.inputs[row][line].answer = null;
+        this.outputs[row][line] = num as InputNum;
+        this.forcusNumber = num as InputNum;
+      }
+      else if (this.inputs[row][line].given === null) {
+        this.inputs[row][line].answer = num as InputNum;
+        this.outputs[row][line] = num as InputNum;
+        this.forcusNumber = num as InputNum;
+      }
     }
-    console.log(this.inputs);
+
+    // 検証
+    for (let i=0; i<9; i+=1) {
+      this.updateValidation(row*9+i);
+      this.updateValidation(i*9+line);
+    }
+    for (const row_ of AppComponent.calcSameFrameIndice(row)) {
+      for (const line_ of AppComponent.calcSameFrameIndice(line)) {
+        this.updateValidation(row_*9+line_);
+      }
+    }
+  };
+
+  updateValidation(index: number): void {
+    const row = Math.floor(index / 9);
+    const line = index % 9;
+    const number = this.outputs[row][line];
+    if (number === null) {
+      this.validations[index] = true;
+      return;
+    }
+
+    // 同列を検証
+    if (this.outputs[row].some((value, index) => index!==line&&value===number)) {
+      this.validations[index] = false;
+      return;
+    };
+
+    // 同行を検証
+    if (this.outputs.map(row => row[line]).some((value, index) => index!==row&&value===number)) {
+      this.validations[index] = false;
+      return;
+    }
+
+    // 残りの同枠を検証
+    for (const row_ of AppComponent.calcSameFrameIndice(row)) {
+      for (const line_ of AppComponent.calcSameFrameIndice(line)) {
+        if (this.outputs[row_][line_]===number) {
+          this.validations[index] = false;
+          return;
+        }
+      }
+    }
+
+    this.validations[index] = true;
   };
 
   ngOnInit(): void {
     if (isPlatformBrowser(this.platformId)) {
-      window.addEventListener('keypress', (event: KeyboardEvent) => this.inputNum(event));
+      window.addEventListener('keyup', (event: KeyboardEvent) => this.inputNum(event));
     }
   };
 
   ngOnDestroy(): void {
     if (isPlatformBrowser(this.platformId)) {
-      window.removeEventListener('keypress', (event: KeyboardEvent) => this.inputNum(event));
+      window.removeEventListener('keyup', (event: KeyboardEvent) => this.inputNum(event));
     }
   }
 }
--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -1,19 +1,37 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { Mode } from '../app.component';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-square',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+  ],
   templateUrl: './square.component.html',
   styleUrl: './square.component.scss'
 })
-export class SquareComponent {
+export class SquareComponent implements OnChanges {
   @Input() context!: Square;
   @Input() mode!: Mode;
   @Input() forcused!: boolean;
+  @Input() highlighted!: boolean;
+  @Input() validation!: boolean;
 
+  display: 'number' | 'memo' = 'number';
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['mode']) {
+      if (!this.context.given && !this.context.answer && this.mode==='memo') {
+        this.display = 'memo';
+      }
+      else {
+        this.display = 'number';
+      }
+    }
+  }
 }
 
 export interface Square {
@@ -21,4 +39,4 @@ export interface Square {
   answer: InputNum;
   memo: string;
 };
-type InputNum = 1|2|3|4|5|6|7|8|9|null;
+export type InputNum = 1|2|3|4|5|6|7|8|9|null;
